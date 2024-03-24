4865e658c90c95f22543c273bf013b646dadfa39
Tom Träskbäck <ttraskback@gmail.com>
2024-03-24 14:51:36 +0200

    Initial commit for max chip

--- src/main.c
+++ src/main.c
@@ -1,30 +1,95 @@
+// Wokwi Custom SPI Chip Example
+//
+// This chip implements a simple ROT13 letter substitution cipher.
+// It receives a string over SPI, and returns the same string with
+// each alphabetic character replaced with its ROT13 substitution.
+//
 // For information and examples see:
 // https://link.wokwi.com/custom-chips-alpha
+//
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2022 Uri Shaked / wokwi.com
 
 #include "wokwi-api.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-  pin_t pin_in;
-  pin_t pin_out;
+  pin_t    cs_pin;
+  uint32_t spi;
+  uint8_t  spi_buffer[1];
+  uint32_t temp_attribute
 } chip_state_t;
 
-static void chip_pin_change(void *user_data, pin_t pin, uint32_t value) {
-  chip_state_t *chip = (chip_state_t*)user_data;
-  printf("Pin change: %d %d\n", pin, value);
-  pin_write(chip->pin_out, !value);
-}
+static void chip_pin_change(void *user_data, pin_t pin, uint32_t value);
+static void chip_spi_done(void *user_data, uint8_t *buffer, uint32_t count);
 
 void chip_init(void) {
   chip_state_t *chip = malloc(sizeof(chip_state_t));
-  chip->pin_in = pin_init("IN", INPUT);
-  chip->pin_out = pin_init("OUT", OUTPUT);
 
-  const pin_watch_config_t config = {
+  chip->temp_attribute = attr_init("relativeTemperature",0);
+  
+  chip->cs_pin = pin_init("CS", INPUT_PULLUP);
+
+  const pin_watch_config_t watch_config = {
     .edge = BOTH,
     .pin_change = chip_pin_change,
     .user_data = chip,
   };
-  pin_watch(chip->pin_in, &config);
+  pin_watch(chip->cs_pin, &watch_config);
+
+  const spi_config_t spi_config = {
+    .sck = pin_init("SCK", INPUT),
+    .miso = pin_init("MISO", INPUT),
+    .done = chip_spi_done,
+    .user_data = chip,
+  };
+  chip->spi = spi_init(&spi_config);
+  
+  printf("SPI Chip initialized!\n");
+
+}
+
+uint8_t rot13(uint8_t value) {
+  const uint8_t ROT = 13;
+  if(value >= 'A' && value <='Z') {
+    return (value + ROT) <= 'Z' ? value + ROT : value - ROT;
+  }
+  if(value >= 'a' && value <= 'z') {
+    return (value + ROT) <= 'z' ? value + ROT : value - ROT;
+  }
+  return value;
+}
+
+void chip_pin_change(void *user_data, pin_t pin, uint32_t value) {
+  chip_state_t *chip = (chip_state_t*)user_data;
+  // Handle CS pin logic
+  if (pin == chip->cs_pin) {
+    if (value == LOW) {
+      printf("SPI chip selected\n");
+      chip->spi_buffer[0] = ' '; // Some dummy data for the first character
+      spi_start(chip->spi, chip->spi_buffer, sizeof(chip->spi_buffer));
+    } else {
+      printf("SPI chip deselected\n");
+      spi_stop(chip->spi);
+    }
+  }
+}
+
+void chip_spi_done(void *user_data, uint8_t *buffer, uint32_t count) {
+  chip_state_t *chip = (chip_state_t*)user_data;
+  if (!count) {
+    // This means that we got here from spi_stop, and no data was received
+    return;
+  }
+
+  // Apply the ROT13 transformation, and store the result in the buffer.
+  // The result will be read back during the next SPI transfer.
+  buffer[0] = rot13(buffer[0]);
+
+  if (pin_read(chip->cs_pin) == LOW) {
+    uint32_t tempearature = attr_read(chip->temp_attribute)
+    // Continue with the next character
+    spi_start(chip->spi, chip->spi_buffer, sizeof(chip->spi_buffer));
+  }
 }
